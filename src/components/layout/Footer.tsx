71bf2b6e4d82732613f0467b80e3c7ef2df107b5
Angelique-D <angelique.didillon@gmail.com>
2024-07-11 16:28:12 +0200

    ✅ update footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer: React.FC = () => {
     return (
-        <footer className="container mx-auto flex">
-            <p className="justify-self-center">By Angelique-D - 2024</p>
+        <footer className=" absolute bottom-0 w-full p-2 flex items-center justify-center bg-blue-950 text-white">
+            <p className="justify-center">By Angelique Didillon - 2024</p>
         </footer>
     );
 };
